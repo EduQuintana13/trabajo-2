c384c445964f36baca348b0537f63d0bd3b1411c
EduQuintana13 <eduquintana13@gmail.com>
2021-10-12 15:45:50 -0300

    listas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import react from "react";
+import Jsx from './components/NavBar'
+import Lista from './components/Lista'
+
+function App() {
+return (
+  <div className="App">
+    <h1>Hola Jsx</h1>
+    <Jsx />
+    <Lista />
+  </div>
+);
+}
+
+export default App;
